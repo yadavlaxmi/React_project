3061fdae5b33491e47a81bd7e80d68ca1d1e132e
navgurukul <laxmiyadav21@navgurukul.org>
2024-07-02 11:20:31 +0530

    accordian single click

---
+++ src/Components/accordian/data.js
@@ -0,0 +1,22 @@
+const data=[
+    {
+    id:"1",
+    question:"what are accordion components?",
+    answer:"Accordian component are user interface element used for organism"
+},
+{
+    id:"2",
+    question:"what are they use",
+    answer:"they are commonly employed in various contexts,including FAQs,product discription"
+},
+{
+    id: "3",
+    question:"Accordion as a musical instrument",
+    answer:"The accordian is a musical instrument with a keyboard and bellows.It produces sourround"
+},{
+    id:"4",
+    question:"can I create an accordion component with a different framework?",
+    answer:"yes of course,it is very possible to create an accordian component with another from"
+},
+];
+export default data;
---
+++ src/Components/accordian/index.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import data from "./data";
+
+export default function Accordian() {
+const [selected, setSelected] = useState(null);
+function handleSingleSelection(getCurrentId) {
+        // console.log(getCurrentId);
+    setSelected(getCurrentId)
+}
+console.log(selected)
+return (
+    <div className="wrapper">
+        <div className="accordian">
+            {data && data.length > 0 ? (
+                data.map((dataItem, index) => (
+                    <div className="item" key={index}>
+                        <div 
+                            onClick={() => handleSingleSelection(dataItem.id)} 
+                            className="title"
+                            >
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
+                            
+                        </div>
+                            {
+                            selected === dataItem.id ?
+                            <div className="content">{dataItem.answer}</div>:null
+                            }
+                        </div>
+                ))
+            ) : (
+                <div>No data found</div>
+        )}
+        </div>
+    </div>
+    );
+}
